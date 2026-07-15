129cd37ba91e08bcc84c65adc6ce04510e5025d9
Circus_Cat <60798189+mirai101@users.noreply.github.com>
2026-07-15 12:10:17 +0800

    Task1 HelloWorld Debug

---
+++ Task1.java
@@ -0,0 +1,16 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+
+clas helloWorldApp { // public class Task1 missing (s) in clas(s), change hellowWorldApp to (Task1)
+    public static void main(String[] args) { // I change agrs to (KamustaMundo)
+        System.out.rintln("Hello World!); // missing (")
+    }
+}
+*/
+
+class Task1{
+    public static void main(String[] KamustaMundo) {
+        System.out.println("Kamusta Mundo!");
+    }
+}
