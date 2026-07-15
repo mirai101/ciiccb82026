c8ceb42494d5e7519f7d88665ee1c48e047497bd
Circus_Cat <60798189+mirai101@users.noreply.github.com>
2026-07-15 12:39:17 +0800

    Update Task1.java

--- Task1/Task1.java
+++ Task1/Task1.java
@@ -1,6 +1,6 @@
 
 class Task1{
-    public static void main(String[] HelloWorld) {
-        System.out.println("Kamusta Mundo!"); // Display the string.
+    public static void main(String[] KamustaMundo) {
+        System.out.println("Kamusta Mundo!");
     }
 }
