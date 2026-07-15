10717cddc198716b5821b0056ecae0e3547a1aea
Circus_Cat <60798189+mirai101@users.noreply.github.com>
2026-07-15 12:20:37 +0800

    Task1 Debug

---
+++ Task1/Task1.java
@@ -0,0 +1,16 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+
+clas helloWorldApp { // public class Task1
+    public static void main(String[] args) { // KamustaMundo
+        System.out.rintln("Hello World!); // ("KamustaMundo").
+    }
+}
+*/
+
+class Task1{
+    public static void main(String[] HelloWorld) {
+        System.out.println("Kamusta Mundo!"); // Display the string.
+    }
+}
